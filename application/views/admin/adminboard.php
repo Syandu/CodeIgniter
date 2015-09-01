4832c694d194a50cd27347e087b47164d437fb18
Syandu <suksesso@gmail.com>
2015-09-01 16:07:05 +0700

    tester sublime

--- application/views/admin/adminboard.php
+++ application/views/admin/adminboard.php
@@ -197,7 +197,7 @@
 
           <li class="treeview">
             <a href="#">
-              <i class="fa fa-dashboard"></i> <span>Manajemen User</span> <i class="fa fa-angle-left pull-right"></i>
+              <i class="fa fa-dashboard"></i> <span>Manajemen Usera</span> <i class="fa fa-angle-left pull-right"></i>
             </a>
             <ul class="treeview-menu">
               <li><a href="index.html"><i class="fa fa-circle-o"></i> Daftar User</a></li>
